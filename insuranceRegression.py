d0632a3789b19ef7b98f76e35fbc19198dee8935
Rishik Sarkar <rishiksarkar123@gmail.com>
2022-11-02 01:13:03 -0400

    Update insuranceRegression.py

--- insuranceRegression.py
+++ insuranceRegression.py
@@ -4,23 +4,17 @@ import pandas as pd
 
 # data = genfromtxt('multiTest1.csv', delimiter=',', dtype='float', skip_header=1)
 
-
-'''
-Read data from "insurance.csv," modify data to float, and convert to NumPy array
-'''
-
-
 file_handler = open("insurance.csv", "r")
-data = pd.read_csv(file_handler, sep = ",")
+data = pd.read_csv(file_handler, sep=",")
 file_handler.close()
 
-sex = {'male': 1., 'female': 2.}
+sex = {'male': 0., 'female': 1.}
 data.sex = [sex[item] for item in data.sex]
 
 smoker = {'no': 0., 'yes': 1.}
 data.smoker = [smoker[item] for item in data.smoker]
 
-region = {'southwest': 1., 'southeast': 2., 'northwest': 3., 'northeast': 4.}
+region = {'southwest': 0., 'southeast': 1., 'northwest': 2., 'northeast': 3.}
 data.region = [region[item] for item in data.region]
 
 df = pd.DataFrame(data)
@@ -40,10 +34,8 @@ y = np.array(data[:, -1])
 cols = len(x[0])
 # print(cols)
 
-
-'''
-Downscale relevant features in training data to 0 - 1 range
-'''
+min_bias = np.min(y)
+print(min_bias)
 
 
 def feature_scaling():
@@ -62,7 +54,7 @@ def feature_scaling():
 
 feature_scaling()
 # print(x)
-w = np.array([1.] * cols)
+w = np.array([0.] * cols)
 # print(w)
 # print(y)
 # print(np.dot(x, w) + 200 - y)
@@ -71,30 +63,18 @@ w = np.array([1.] * cols)
 # print(np.dot(np.sum(np.dot(x, w) + 200 - y), x[:, 0]))
 # print(np.sum(np.dot(np.sum(np.dot(x, w) + 200 - y), x[:, 0])))
 
-'''
-Find partial derivative of cost function for w(j)
-d/dw(j) = (1/m) * sigma((y_hat - y)^2 * x(j))
-'''
+# print(w, x, y)
+print(np.dot(x, w) + 0 - y, x[:, 0])
+print(np.dot(np.dot(x, w) + 0 - y, x[:, 0]))
+print(np.dot(np.dot(x, w) + 0 - y, x[:, 0]) / len(x))
 
 
 def find_deriv_w(weights, bias, j):
-    return np.sum(np.dot(np.sum(np.dot(x, weights) + bias - y), x[:, j])) / len(x)
-
-
-'''
-Find partial derivative of cost function for b
-d/db = (1/m) * sigma((y_hat - y)^2)
-'''
+    return np.dot(np.dot(x, weights) + bias - y, x[:, j]) / len(x)
 
 
 def find_deriv_b(weights, bias):
     return np.sum(np.dot(x, weights) + bias - y) / len(x)
-
-
-'''
-Perform gradient descent to minimize cost function
-Simultaneously update w for all j and b by decreasing respective partial derivative * alpha from current value
-'''
 
 
 def gradient_descent(weights, bias, alpha, epoch):
@@ -103,22 +83,12 @@ def gradient_descent(weights, bias, alpha, epoch):
         for j in range(cols):
             weights[j] = weights[j] - alpha * find_deriv_w(weights, bias, j)
         bias = bias - alpha * find_deriv_b(temp, bias)
-        # print(weights, bias)
+        print("w: ", weights, "b: ", bias)
     return weights, bias
-
-
-'''
-Predict health insurance value using w and b
-'''
 
 
 def predict_value(features, weights, bias):
     return np.sum(np.dot(features, weights) + bias)
-
-
-'''
-Downscale relevant features in test data to 0 - 1 range
-'''
 
 
 def feature_scale_f(features):
@@ -128,21 +98,15 @@ def feature_scale_f(features):
     return features
 
 
-weight, b = gradient_descent(w, 0, 0.001, 1000)
+weight, b = gradient_descent(w, min_bias, 3, 10000)
 print("w: ", weight, "b: ", b)
 
-
-'''
-Ask for input features for testing
-'''
-
-
 age = float(input("Age: "))
-sex = float(input("Sex (1/2): "))
+sex = float(input("Sex (0/1): "))
 bmi = float(input("BMI: "))
 children = float(input("# Children: "))
 smoker = float(input("Smoker (0/1): "))
-region = float(input("Region (1/2/3/4 = SW/SE/NW/NE): "))
+region = float(input("Region (0/1/2/3 = SW/SE/NW/NE): "))
 
 f = np.array([age, sex, bmi, children, smoker, region])
 f = feature_scale_f(f)
