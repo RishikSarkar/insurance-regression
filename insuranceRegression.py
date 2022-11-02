ef698cbe872a9aca35da5674d258964c378a0a18
Rishik Sarkar <rishiksarkar123@gmail.com>
2022-11-02 04:17:01 -0400

    Update insuranceRegression.py

--- insuranceRegression.py
+++ insuranceRegression.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 # data = genfromtxt('multiTest1.csv', delimiter=',', dtype='float', skip_header=1)
 
+
+'''
+Read data from "insurance.csv," modify data to float, and convert to NumPy array
+'''
+
+
 file_handler = open("insurance.csv", "r")
 data = pd.read_csv(file_handler, sep=",")
 file_handler.close()
@@ -38,6 +44,11 @@ min_bias = np.min(y)
 print(min_bias)
 
 
+'''
+Downscale relevant features in training data to 0 - 1 range
+'''
+
+
 def feature_scaling():
     for i in range(cols):
         maxx = np.max(x[:, i])
@@ -63,18 +74,30 @@ w = np.array([0.] * cols)
 # print(np.dot(np.sum(np.dot(x, w) + 200 - y), x[:, 0]))
 # print(np.sum(np.dot(np.sum(np.dot(x, w) + 200 - y), x[:, 0])))
 
-# print(w, x, y)
-print(np.dot(x, w) + 0 - y, x[:, 0])
-print(np.dot(np.dot(x, w) + 0 - y, x[:, 0]))
-print(np.dot(np.dot(x, w) + 0 - y, x[:, 0]) / len(x))
+'''
+Find partial derivative of cost function for w(j)
+d/dw(j) = (1/m) * sigma((y_hat - y)^2 * x(j))
+'''
 
 
 def find_deriv_w(weights, bias, j):
     return np.dot(np.dot(x, weights) + bias - y, x[:, j]) / len(x)
 
 
+'''
+Find partial derivative of cost function for b
+d/db = (1/m) * sigma((y_hat - y)^2)
+'''
+
+
 def find_deriv_b(weights, bias):
     return np.sum(np.dot(x, weights) + bias - y) / len(x)
+
+
+'''
+Perform gradient descent to minimize cost function
+Simultaneously update w for all j and b by decreasing respective partial derivative * alpha from current value
+'''
 
 
 def gradient_descent(weights, bias, alpha, epoch):
@@ -87,8 +110,18 @@ def gradient_descent(weights, bias, alpha, epoch):
     return weights, bias
 
 
+'''
+Predict health insurance value using w and b
+'''
+
+
 def predict_value(features, weights, bias):
-    return np.sum(np.dot(features, weights) + bias)
+    return np.dot(features, weights) + bias
+
+
+'''
+Downscale relevant features in test data to 0 - 1 range
+'''
 
 
 def feature_scale_f(features):
@@ -101,13 +134,20 @@ def feature_scale_f(features):
 weight, b = gradient_descent(w, min_bias, 3, 10000)
 print("w: ", weight, "b: ", b)
 
-age = float(input("Age: "))
-sex = float(input("Sex (0/1): "))
-bmi = float(input("BMI: "))
-children = float(input("# Children: "))
-smoker = float(input("Smoker (0/1): "))
-region = float(input("Region (0/1/2/3 = SW/SE/NW/NE): "))
 
-f = np.array([age, sex, bmi, children, smoker, region])
-f = feature_scale_f(f)
-print(predict_value(f, weight, b))
+'''
+Ask for input features for testing
+'''
+
+
+while True:
+    age = float(input("Age: "))
+    sex = float(input("Sex (0/1): "))
+    bmi = float(input("BMI: "))
+    children = float(input("# Children: "))
+    smoker = float(input("Smoker (0/1): "))
+    region = float(input("Region (0/1/2/3 = SW/SE/NW/NE): "))
+
+    f = np.array([age, sex, bmi, children, smoker, region])
+    f = feature_scale_f(f)
+    print(predict_value(f, weight, b))
